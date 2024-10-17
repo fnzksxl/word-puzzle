9858800f0507e2a3ce021a651561b09176c92c66
fnzksxl <fnzksxl@gmail.com>
2024-10-17 18:19:13 +0900

    #1 Feat: 단어 길이 인덱스 생성 및 단어 최대 길이 축소

--- app/models.py
+++ app/models.py
@@ -6,10 +6,11 @@ from app.database import Base
 class BaseMin:
     id = Column(Integer, primary_key=True, index=True)
 
+
 class WordInfo(BaseMin, Base):
     __tablename__ = "wordinfo"
 
-    word = Column(VARCHAR(7), nullable=False)
+    word = Column(VARCHAR(5), nullable=False)
     desc = Column(VARCHAR(200), nullable=False)
-    len = Column(Integer, nullable=False)
+    len = Column(Integer, nullable=False, index=True)
     pos = Column(VARCHAR(7), nullable=False)
