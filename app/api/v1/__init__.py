a237dc3098fd4d20b608742c34e07099ed864ea5
fnzksxl <fnzksxl@gmail.com>
2024-10-25 00:08:31 +0900

    #2 Feat: Auth 라우터 V1라우터에 추가

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
-from .puzzle import router as puzzle_router
+from .puzzle.controller import router as puzzle_router
+from .auth.controller import router as auth_router
 
 router = APIRouter()
 
 router.include_router(puzzle_router, prefix="/v1")
+router.include_router(auth_router, prefix="/v1")
