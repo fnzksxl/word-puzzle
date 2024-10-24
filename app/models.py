93b7382d4c5806b32f875de63099f238a0aa6b95
fnzksxl <fnzksxl@gmail.com>
2024-10-24 23:46:20 +0900

    #2 Feat: User 테이블 생성

--- app/models.py
+++ app/models.py
@@ -1,11 +1,16 @@
 import uuid
-from sqlalchemy import Column, Integer, VARCHAR, JSON, ForeignKey
+from sqlalchemy import Column, Integer, VARCHAR, JSON, ForeignKey, DATETIME, func
 
 from app.database import Base
 
 
 class BaseMin:
     id = Column(Integer, primary_key=True, index=True)
+
+
+class BaseDate(BaseMin):
+    created_at = Column(DATETIME, default=func.now())
+    updated_at = Column(DATETIME, default=func.now(), onupdate=func.now())
 
 
 class WordInfo(BaseMin, Base):
@@ -30,3 +35,15 @@ class PuzzleAnswer(BaseMin, Base):
     puzzle_id = Column(Integer, ForeignKey("puzzle.id"), nullable=False)
     word_id = Column(Integer, ForeignKey("wordinfo.id"), nullable=False)
     num = Column(Integer, nullable=False)
+
+
+class User(BaseDate, Base):
+    __tablename__ = "user"
+
+    email = Column(VARCHAR(20), unique=True, nullable=False)
+    password = Column(VARCHAR(70), nullable=True)
+    nickname = Column(VARCHAR(10), nullable=False)
+    solved = Column(Integer, default=0)
+
+    def as_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
