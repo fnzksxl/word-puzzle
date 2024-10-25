f775f9e103fdb9de2b6105861ba485e9ffe053f6
fnzksxl <fnzksxl@gmail.com>
2024-10-25 23:04:44 +0900

    #2 Feat: Login, Register 의존성 함수 분리

--- app/api/v1/auth/dependancy.py
+++ app/api/v1/auth/dependancy.py
@@ -2,13 +2,19 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from app.database import get_db
-from .schema import GeneralAuthModel
+from .schema import GeneralRegisterModel, GeneralLoginModel
 from .service import GeneralAuthService
 
 
-def get_general_auth_service(
-    auth_data: GeneralAuthModel, db: Session = Depends(get_db)
+def get_general_auth_service_register(
+    auth_data: GeneralRegisterModel, db: Session = Depends(get_db)
 ) -> GeneralAuthService:
     return GeneralAuthService(
         email=auth_data.email, password=auth_data.password, nickname=auth_data.nickname, db=db
     )
+
+
+def get_general_auth_service_login(
+    auth_data: GeneralLoginModel, db: Session = Depends(get_db)
+) -> GeneralAuthService:
+    return GeneralAuthService(email=auth_data.email, password=auth_data.password, db=db)
