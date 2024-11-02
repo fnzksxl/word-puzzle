f38b2c029ab0407c6c24b291023bfb5839646c59
fnzksxl <fnzksxl@gmail.com>
2024-11-02 21:55:39 +0900

    #12 Feat: 4개 퍼즐 DB 삽입 fixture 추가

--- test/puzzle/conftest.py
+++ test/puzzle/conftest.py
@@ -21,3 +21,23 @@ async def puzzle(session):
 
     session.bulk_insert_mappings(models.PuzzleAnswer, insert_data)
     session.commit()
+
+
+@pytest_asyncio.fixture
+async def four_puzzles(session):
+    with open("test/example/example_map.json", "r", encoding="utf-8") as f:
+        json_data = json.load(f)
+    _map = json_data.get("map", None)
+    _answers = json_data.get("desc", None)
+    for _ in range(4):
+        map_row = models.Puzzle(puzzle=_map)
+        session.add(map_row)
+        session.flush()
+
+        insert_data = [
+            {"puzzle_id": map_row.id, "word_id": desc["id"], "num": desc["num"]}
+            for desc in _answers
+        ]
+
+        session.bulk_insert_mappings(models.PuzzleAnswer, insert_data)
+        session.commit()
