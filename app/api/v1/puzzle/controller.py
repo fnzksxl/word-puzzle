1f0998484857ca9680acf6f1167b08e98b7b824f
fnzksxl <fnzksxl@gmail.com>
2024-11-02 22:01:37 +0900

    #12 Feat: 퍼즐목록 페이지네이션 엔드포인트 추가

--- app/api/v1/puzzle/controller.py
+++ app/api/v1/puzzle/controller.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, status, Depends
+from typing import Optional
+
 from .service import PuzzleCreateService, PuzzleReadService
 from .schema import PuzzleSize
 
@@ -19,9 +21,22 @@ async def create_puzzle(
 
 
 @router.get(
-    "/{puzzle_id}", status_code=status.HTTP_200_OK, description="PUZZLE_ID인 십자말풀이판 반환"
+    "/search/{puzzle_id}",
+    status_code=status.HTTP_200_OK,
+    description="PUZZLE_ID인 십자말풀이판 반환",
 )
 async def read_puzzle(
     puzzle_id: int, puzzle_service: PuzzleReadService = Depends(PuzzleReadService)
 ):
-    return await puzzle_service.read_puzzle_from_db_by_id()
+    return await puzzle_service.read_puzzle_from_db_by_id(puzzle_id=puzzle_id)
+
+
+@router.get(
+    "/paginated",
+    status_code=status.HTTP_200_OK,
+    description="MAIN 페이지 데이터 전송, 퍼즐 목록 커서기반 페이지네이션으로 반환",
+)
+async def read_puzzles_on_main(
+    key: Optional[int] = None, puzzle_service: PuzzleReadService = Depends(PuzzleReadService)
+):
+    return await puzzle_service.get_puzzle_list_by_pagination(key=key)
