73470aef24f504771e80fa1dcb275660ac34022f
fnzksxl <fnzksxl@gmail.com>
2024-11-07 18:30:43 +0900

    #16 Feat: Puzzle 생성 방식 변화로 인한 fixture 수정

--- test/puzzle/conftest.py
+++ test/puzzle/conftest.py
@@ -9,17 +9,37 @@ async def puzzle(session):
     with open("test/example/example_map.json", "r", encoding="utf-8") as f:
         json_data = json.load(f)
     _map = json_data.get("map", None)
-    _answers = json_data.get("desc", None)
+    across = json_data.get("across", None)
+    down = json_data.get("down", None)
 
     map_row = models.Puzzle(puzzle=_map, is_exposed=True)
     session.add(map_row)
     session.flush()
 
-    insert_data = [
-        {"puzzle_id": map_row.id, "word_id": desc["id"], "num": desc["num"]} for desc in _answers
+    across_data = [
+        {
+            "puzzle_id": map_row.id,
+            "start_x": desc["startpoint"][0],
+            "start_y": desc["startpoint"][1],
+            "dir": "across",
+            "word_id": 1,
+            "num": desc["num"],
+        }
+        for desc in across
+    ]
+    down_data = [
+        {
+            "puzzle_id": map_row.id,
+            "start_x": desc["startpoint"][0],
+            "start_y": desc["startpoint"][1],
+            "dir": "down",
+            "word_id": 1,
+            "num": desc["num"],
+        }
+        for desc in down
     ]
 
-    session.bulk_insert_mappings(models.PuzzleAnswer, insert_data)
+    session.bulk_insert_mappings(models.PuzzleAnswer, across_data + down_data)
     session.commit()
 
     return {"id": map_row.id, "name": map_row.name}
@@ -30,19 +50,41 @@ async def four_puzzles(session):
     with open("test/example/example_map.json", "r", encoding="utf-8") as f:
         json_data = json.load(f)
     _map = json_data.get("map", None)
-    _answers = json_data.get("desc", None)
+    across = json_data.get("across", None)
+    down = json_data.get("down", None)
+
     for _ in range(4):
         map_row = models.Puzzle(puzzle=_map, is_exposed=True)
         session.add(map_row)
         session.flush()
 
-        insert_data = [
-            {"puzzle_id": map_row.id, "word_id": desc["id"], "num": desc["num"]}
-            for desc in _answers
+        across_data = [
+            {
+                "puzzle_id": map_row.id,
+                "start_x": desc["startpoint"][0],
+                "start_y": desc["startpoint"][1],
+                "dir": "across",
+                "word_id": 1,
+                "num": desc["num"],
+            }
+            for desc in across
+        ]
+        down_data = [
+            {
+                "puzzle_id": map_row.id,
+                "start_x": desc["startpoint"][0],
+                "start_y": desc["startpoint"][1],
+                "dir": "down",
+                "word_id": 1,
+                "num": desc["num"],
+            }
+            for desc in down
         ]
 
-        session.bulk_insert_mappings(models.PuzzleAnswer, insert_data)
+        session.bulk_insert_mappings(models.PuzzleAnswer, across_data + down_data)
         session.commit()
+
+    return {"id": map_row.id, "name": map_row.name}
 
 
 @pytest_asyncio.fixture
@@ -50,15 +92,37 @@ async def unexposed_puzzle(session):
     with open("test/example/example_map.json", "r", encoding="utf-8") as f:
         json_data = json.load(f)
     _map = json_data.get("map", None)
-    _answers = json_data.get("desc", None)
+    across = json_data.get("across", None)
+    down = json_data.get("down", None)
 
     map_row = models.Puzzle(puzzle=_map)
     session.add(map_row)
     session.flush()
 
-    insert_data = [
-        {"puzzle_id": map_row.id, "word_id": desc["id"], "num": desc["num"]} for desc in _answers
+    across_data = [
+        {
+            "puzzle_id": map_row.id,
+            "start_x": desc["startpoint"][0],
+            "start_y": desc["startpoint"][1],
+            "dir": "across",
+            "word_id": 1,
+            "num": desc["num"],
+        }
+        for desc in across
+    ]
+    down_data = [
+        {
+            "puzzle_id": map_row.id,
+            "start_x": desc["startpoint"][0],
+            "start_y": desc["startpoint"][1],
+            "dir": "down",
+            "word_id": 1,
+            "num": desc["num"],
+        }
+        for desc in down
     ]
 
-    session.bulk_insert_mappings(models.PuzzleAnswer, insert_data)
+    session.bulk_insert_mappings(models.PuzzleAnswer, across_data + down_data)
     session.commit()
+
+    return {"id": map_row.id, "name": map_row.name}
