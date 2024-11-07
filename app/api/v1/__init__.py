162aeca9c319d17f5032be736b1e34b24718e18e
fnzksxl <fnzksxl@gmail.com>
2024-11-07 18:47:18 +0900

    #16 user 라우터 등록

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from .puzzle.controller import router as puzzle_router
 from .auth.controller import router as auth_router
+from .user.controller import router as user_router
 
 router = APIRouter()
 
 router.include_router(puzzle_router, prefix="/v1")
 router.include_router(auth_router, prefix="/v1")
+router.include_router(user_router, prefix="/v1")
